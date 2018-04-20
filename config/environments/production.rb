25e8cc75edab7a44097957f31b6c228bc2317813
pkhilenko <pkhilenko@yandex.ru>
2018-04-20 15:58:07 +0600

    domain gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'heroku.com',
+    domain:               'gmail.com',
     user_name:            'petrablog.ru@gmail.com', # 'pkhilenko@gmail.com',
     password:             'c-C_1965',
     authentication:       'plain',
