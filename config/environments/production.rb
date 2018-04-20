882864d0b98fdd8be4d4a007b3a2b5565d5093f6
pkhilenko <pkhilenko@yandex.ru>
2018-04-20 15:43:27 +0600

    ActionMailer::Base

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'https://startbootstrap-freelancer.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
+  ActionMailer::Base.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'heroku.com',
