2b6338210762fe09a52f44903164e5399616c8ea
pkhilenko <pkhilenko@yandex.ru>
2018-04-20 15:26:04 +0600

    heroku.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,13 +92,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'heroku.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'localhost:3000',
+    domain:               'heroku.com',
     user_name:            'petrablog.ru@gmail.com', # 'pkhilenko@gmail.com',
     password:             'c-C_1965',
     authentication:       'plain',
